8d7c96b8ba66f7091379fcb3ffa327509976aaea
alexm <alexander.matienko@gmail.com>
2016-06-20 16:27:01 +0300

    h2 + strong

--- templates/common/item-taxonomy-header.php
+++ templates/common/item-taxonomy-header.php
@@ -6,12 +6,12 @@ if (!empty($mprm_view_args['categ_name']) && 'none' !== $mprm_view_args['categ_n
 		?>
 		<div class="mprm-header with-image" style="background-image: url('<?php echo (mprm_has_category_image()) ? mprm_get_category_image('large') : 'none'; ?>')">
 			<div class="mprm-header-content">
-				<h1 class="mprm-title"><?php if (!empty($icon)): ?><i class="mprm-icon <?php echo $icon ?>"></i><?php endif; ?><?php echo $title ?></h1>
+				<h2 class="mprm-title"><?php if (!empty($icon)): ?><i class="mprm-icon <?php echo $icon ?>"></i><?php endif; ?><?php echo $title ?></h2>
 			</div>
 		</div>
 	<?php } else { ?>
 		<div class="mprm-header only-text">
-			<h1 class="mprm-title"><?php echo $title ?></h1>
+			<h2 class="mprm-title"><?php echo $title ?></h2>
 		</div>
 		<?php
 	}
