68e7893b35fb8c303a9ce353e93782c57a792c42
Ignatmg <ignatmg@gmail.com>
2016-06-21 15:49:21 +0300

    fix for firefox column-count

--- templates/shortcodes/menu/index.php
+++ templates/shortcodes/menu/index.php
@@ -6,17 +6,9 @@ $i = 1;
 	<div class="mprm-columns-count-<?php echo $col ?>">
 		<?php foreach ($term_data as $term => $data) {
 			if (in_array($view, array('list', 'grid'))) {
-
 				render_term_header($data);
-
-				list($post, $i) = create_grid_by_posts($data, $col);
-
-			} elseif ($view == 'simple-list') { ?>
-
-				<?php $last_key = array_search(end($term_data), $term_data);
-				if ((empty($current_term) || $current_term != $data['term']) && !empty($data['term'])) { ?>
-					<!--					<div class="--><?php //echo get_column_class($col, 'simple-list'); ?><!--">-->
-				<?php }
+				create_grid_by_posts($data, $col);
+			} elseif ($view == 'simple-list') {
 
 				render_term_header($data);
 
@@ -25,8 +17,7 @@ $i = 1;
 						setup_postdata($post);
 						mprm_set_menu_item($post->ID); ?>
 						<div class="mprm-column-count">
-							<?php
-							render_current_html(); ?>
+							<?php render_current_html(); ?>
 						</div>
 						<?php
 						wp_reset_postdata();
@@ -36,21 +27,16 @@ $i = 1;
 						setup_postdata($post); ?>
 						<div <?php post_class("mprm-column-count"); ?>>
 							<?php
+
 							mprm_set_menu_item($post->ID);
 							render_current_html();
 							wp_reset_postdata();
+
 							?>
 						</div>
 					<?php endforeach;
 				}
-
-				if ((empty($current_term) || $current_term != $data['term']) && !empty($data['term'])) { ?>
-					<!--					</div>-->
-					<?php $current_term = $data['term'];
-				}
-				$i++; ?>
-
-			<?php }
+			}
 		} ?>
 	</div>
 </div>
