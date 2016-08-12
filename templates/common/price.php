732913d54b40cf720ba71bca3a076c3c6fcae526
Ignatmg <ignatmg@gmail.com>
2016-08-12 17:15:35 +0300

    -  list-view - margin 8px 0 - Nutritional

--- templates/common/price.php
+++ templates/common/price.php
@@ -4,5 +4,5 @@ if (empty($price)) {
 }
 if (!empty($price)) :
 	$price = mprm_currency_filter(mprm_format_amount($price)); ?>
-	<div class="mprm-content-container"><span class="mprm-price"><?php echo $price ?></span></div>
+	<div class="mprm-content-container"><div class="mprm-price-container"><span class="mprm-price"><?php echo $price ?></span></div>
 <?php endif;
