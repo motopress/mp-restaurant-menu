bea9ba51edb4ae9595cded4762da0965a9207e50
Ignatmg <ignatmg@gmail.com>
2016-08-23 16:02:44 +0300

    DFRMP-234

--- classes/class-hooks.php
+++ classes/class-hooks.php
@@ -1350,7 +1350,7 @@ class Hooks extends Core {
 	public function filter_options($args) {
 
 		if (isset($args['main']['category_view'])) {
-			if (mprm_get_option('template_mode') == 'theme' || current_theme_supports('mp-restaurant-menu')) {
+			if (mprm_get_option('template_mode') == 'theme') {
 				unset($args['main']['category_view']);
 			}
 		}
