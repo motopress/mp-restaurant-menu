f5a713a5b05a6a024ed896d36df7f526b8e66a80
Ignatmg <ignatmg@gmail.com>
2016-08-23 17:59:58 +0300

    - fix template mode

--- classes/class-hooks.php
+++ classes/class-hooks.php
@@ -1350,7 +1350,7 @@ class Hooks extends Core {
 	public function filter_options($args) {
 
 		if (isset($args['main']['category_view'])) {
-			if (mprm_get_option('template_mode') == 'theme' || mprm_get_option('template_mode') == '') {
+			if (mprm_get_option('template_mode', 'theme') == 'theme') {
 				unset($args['main']['category_view']);
 			}
 		}
