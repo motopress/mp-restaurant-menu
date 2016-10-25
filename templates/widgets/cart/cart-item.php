a721566f9fc3c75b84db4076f2dc9e1028f91745
Ignatmg <ignatmg@gmail.com>
2016-10-25 15:52:37 +0300

    - DFRMP-281

--- templates/widgets/cart/cart-item.php
+++ templates/widgets/cart/cart-item.php
@@ -3,7 +3,7 @@
 <li class="mprm-cart-item">
 	<?php apply_filters('mprm-cart-item-data-before', $item) ?>
 	<span class="mprm-cart-item-title"><a href="{cart_item_url}">{item_title}</a></span>
-	<span class="mprm-cart-item-separator">-</span><span class="mprm-cart-item-quantity">&nbsp;{item_quantity}&nbsp;:&nbsp;</span><span class="mprm-cart-item-price">&nbsp;{item_amount}&nbsp;</span>
+	<span class="mprm-cart-item-separator">-</span><span class="mprm-cart-item-quantity">&nbsp;{item_quantity}&nbsp;&times;</span><span class="mprm-cart-item-price">&nbsp;{item_amount}&nbsp;</span>
 	<a href="{remove_url}" data-cart-item="{cart_item_id}" data-menu-item-id="{item_id}" data-action="mprm_remove_from_cart" class="mprm-remove-from-cart"></a>
 	<?php apply_filters('mprm-cart-item-data-after', $item) ?>
 </li>
