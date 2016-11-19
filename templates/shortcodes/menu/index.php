621e1407e6d7d8e4fcb5022caa1db3398024fa1e
Ignatmg <ignatmg@gmail.com>
2016-11-19 11:45:36 +0200

    Fix unknown error

--- templates/shortcodes/menu/index.php
+++ templates/shortcodes/menu/index.php
@@ -3,11 +3,10 @@ $term_data = mprm_get_term_menu_items();
 ?>
 <div class="<?php echo apply_filters('mprm-shortcode-items-wrapper-class', 'mprm-container mprm-shortcode-items mprm-view-' . $view . mprm_popular_theme_class()) ?>">
 	<?php if ($view == 'simple-list'){ ?>
-
 	<div class="mprm-columns-count-<?php echo $col ?> <?php echo empty($data['term']) ? 'mprm-all-items' : '' ?>">
 		<?php }
-
 		foreach ($term_data as $term => $data) {
+
 			if (in_array($view, array('list', 'grid'))) {
 				render_term_header($data);
 				create_grid_by_posts($data, $col);
@@ -19,47 +18,38 @@ $term_data = mprm_get_term_menu_items();
 						<?php render_term_header($data); ?>
 					</div>
 				<? }
-				if (is_array($data['posts'])) {
+				if (!empty($data['term'])) {
+					$array_keys = array_keys($data['posts']);
+					$last = end($array_keys);
+					$first = reset($array_keys);
+				} else {
+					$class = '';
+				}
 
-					if (!empty($data['term'])) {
-						$array_keys = array_keys($data['posts']);
-						$first = reset($array_keys);
-						$last = end($array_keys);
-					} else {
-						$class = '';
+				foreach ($data['posts'] as $key => $post) {
+
+					if (isset($first) && isset($last)) {
+						if ($key === $first) {
+							$class = ' mprm-first';
+						} elseif ($key === $last) {
+							$class = ' mprm-last';
+						} else {
+							$class = '';
+						}
 					}
 
-					foreach ($data['posts'] as $key => $post) :
-						if (isset($first) && isset($last)) {
-							if ($key === $first) {
-								$class = ' mprm-first';
-							} elseif ($key === $last) {
-								$class = ' mprm-last';
-							} else {
-								$class = '';
-							}
-						}
+					setup_postdata($post);
 
-						setup_postdata($post);
+					mprm_set_menu_item($post->ID); ?>
 
-						mprm_set_menu_item($post->ID); ?>
-
-						<div class="<?php echo apply_filters('mprm-simple-view-column', 'mprm-simple-view-column') . $class; ?> ">
-							<?php render_current_html(); ?>
-						</div>
-
-						<?php
-
-						wp_reset_postdata();
-
-					endforeach;
+					<div class="<?php echo apply_filters('mprm-simple-view-column', 'mprm-simple-view-column') . $class; ?> ">
+						<?php render_current_html(); ?>
+					</div>
+					<?php wp_reset_postdata();
 				}
 			}
 		}
-
 		if ($view == 'simple-list'){ ?>
 	</div>
-
 <?php } ?>
-
 </div>
--- templates/widgets/menu/index.php
+++ templates/widgets/menu/index.php
@@ -1,74 +1,52 @@
 <?php global $post;
 
 before_mprm_widget();
-
 the_mprm_widget_title();
-
 $term_data = mprm_get_term_menu_items(); ?>
 
 	<div class="<?php echo apply_filters('mprm-widget-items-wrapper-class', 'mprm-container mprm-widget-items mprm-view-' . $view . mprm_popular_theme_class()) ?>">
 		<?php if ($view == 'simple-list'){ ?>
 		<div class="mprm-columns-count-<?php echo $col ?> <?php echo empty($data['term']) ? 'mprm-all-items' : '' ?>">
-			<?php } ?>
-
-			<?php
-
+			<?php }
 			foreach ($term_data as $term => $data) {
-
 				if (in_array($view, array('list', 'grid'))) {
 					$last_key = array_search(end($data['posts']), $data['posts']);
-
 					render_term_header($data);
-
 					create_grid_by_posts($data, $col);
-
 				} elseif ($view == 'simple-list') {
-
 					if (!empty($data['term'])) { ?>
 						<div class="<?php echo apply_filters('mprm-simple-view-column', 'mprm-simple-view-column') ?>">
 							<?php render_term_header($data); ?>
 						</div>
 					<? }
-					if (is_array($data['posts'])) {
-
-						if (!empty($data['term'])) {
-							$array_keys = array_keys($data['posts']);
-							$first = reset($array_keys);
-							$last = end($array_keys);
-						} else {
-							$class = '';
-						}
-
-						foreach ($data['posts'] as $key => $post) :
-							if (isset($first) && isset($last)) {
-								if ($key === $first) {
-									$class = ' mprm-first';
-								} elseif ($key === $last) {
-									$class = ' mprm-last';
-								} else {
-									$class = '';
-								}
+					if (!empty($data['term'])) {
+						$array_keys = array_keys($data['posts']);
+						$last = end($array_keys);
+						$first = reset($array_keys);
+					} else {
+						$class = '';
+					}
+					foreach ($data['posts'] as $key => $post) {
+						if (isset($first) && isset($last)) {
+							if ($key === $first) {
+								$class = ' mprm-first';
+							} elseif ($key === $last) {
+								$class = ' mprm-last';
+							} else {
+								$class = '';
 							}
-
-							setup_postdata($post);
-
-							mprm_set_menu_item($post->ID); ?>
-
-							<div class="<?php echo apply_filters('mprm-simple-view-column', 'mprm-simple-view-column') . $class; ?> ">
-								<?php render_current_html(); ?>
-							</div>
-
-							<?php
-
-							wp_reset_postdata();
-
-						endforeach;
+						}
+						setup_postdata($post);
+						mprm_set_menu_item($post->ID); ?>
+						<div class="<?php echo apply_filters('mprm-simple-view-column', 'mprm-simple-view-column') . $class; ?> ">
+							<?php render_current_html(); ?>
+						</div>
+						<?php wp_reset_postdata();
 					}
 				}
-			} ?>
-			<?php if ($view == 'simple-list'){ ?>
+			}
+			if ($view == 'simple-list'){ ?>
 		</div>
 	<?php } ?>
 	</div>
-
 <?php after_mprm_widget() ?>
